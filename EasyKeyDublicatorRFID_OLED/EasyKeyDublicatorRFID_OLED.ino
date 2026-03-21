b8e0d528df2c026c1a8c7d7bc3c9caa71e1d2864
sQueezy1332 <47674362+sQueezy1332@users.noreply.github.com>
2026-03-22 00:40:01 +0300

    Update EasyKeyDublicatorRFID_OLED.ino

--- EasyKeyDublicatorRFID_OLED/EasyKeyDublicatorRFID_OLED.ino
+++ EasyKeyDublicatorRFID_OLED/EasyKeyDublicatorRFID_OLED.ino
@@ -5,13 +5,99 @@
   Автор: HERR DIY, AlexMalov, 2019
   v 3.2 fix cyfral bug
 */
-
 #include "main_header.h"
+//FFAA A000 6FC2 9000
+void print_key(const byte buf[]);
+void do_something(byte err) {
+	if (err > ERROR_RFID_HEADER) Serial.write(err);
+}
 
-int main(void) {
-	setup();
-	int error;
-	for (;;) {
+__ATTR_NORETURN__ void rfid_test() {
+	//*reinterpret_cast<uint64_t*>(keyID) = 0x365A1140BEFF;
+	//pinMode(COMP_PIN, INPUT_PULLUP);
+	pinMode(LED_BUILTIN, OUTPUT);
+	pinMode(D2, OUTPUT);
+	//pinMode(6, INPUT_PULLUP); //comparator invert input 0.1v
+	Serial.begin(256000);
+	Serial.println(__TIMESTAMP__);
+	rfid_init();
+	rfid_pwm_enable();
+	//extern void dac_init();dac_init();
+#if defined __LGT8F__
+	C0SR = 0; //comparator enable
+	C0XR = _BV(C0OE) | _BV(C0FEN) | 0b00; //comparator output pin D2; filter 32us (dont work)
+#else
+	ACSR = 0;
+#endif
+	byte key_buf[1][8]{};
+	const byte buf_cap = sizeof key_buf / sizeof key_buf[0]; int ret;
+	for (int keyIndex = 0;;) {
+		uint32_t timer = millis();
+		while (keyIndex < buf_cap) {
+			if (!readEM_Marine(key_buf[keyIndex])) {
+				if (*(uint32_t*)(key_buf[keyIndex] + 1) == 0) continue;
+				++keyIndex; PINB |= _BV(PB5);
+			}
+		} uint32_t delta = millis() - timer;
+		keyIndex = 0;
+		for (int i = 0; i < buf_cap; i++) {
+			print_key(key_buf[i]);
+		}
+		Serial.print('\t'); Serial.print(delta); //continue;
+		DEBUGLN();
+		delay(1000);
+		ret = keyCompare(keyID);
+		if (ret == NOERROR) {
+			DEBUGLN("\nKEY_SAME"); continue;
+		}
+		else if (ret != KEY_MISMATCH) {
+			DEBUGLN(ret); continue;
+		}//ret = EM4305; goto write;
+		ret = getRFIDtype();
+		DEBUG(F("\nRFID Type: ")); DEBUGLN(ret);
+		if (ret < UNKNOWN_TYPE) continue;
+		DEBUG(F("Vendor IDs: ")); DEBUG(*(uint32_t*)Buffer, HEX); 
+		if (ret == UNKNOWN_TYPE)
+			{ DEBUG(" | "); DEBUG(*(uint32_t*)(Buffer + 4), HEX); }
+		DEBUGLN();
+	write:
+		switch (ret) {
+		case T5557:
+			ret = write_T5557(keyID); break;
+		case EM4305:
+			ret = write_em4305(keyID); break;
+		}
+		//ret = writeRfid(keyID);
+		switch (ret)
+		{
+		case NOERROR: DEBUGLN("\nSuccess"); break;
+		//case KEY_SAME: DEBUGLN("\nKEY_SAME");break;
+		case ERROR_RFID_COMP_TIMEOUT: DEBUGLN("\nCOMP_TIMEOUT"); continue;
+		case KEY_MISMATCH: DEBUGLN("\nKEY_MISMATCH"); continue;
+		//case UNKNOWN_TYPE: DEBUGLN("\nUNKNOWN_TYPE"); continue;
+		default: DEBUGLN(ret);
+		}
+		delay(3000);
+	}
+
+}
+
+void print_key(const byte buf[]) {
+	//Serial.println();
+	for (byte i = 5; i <= 5; --i) {
+#ifdef ARDUINO_ARCH_ESP32
+		Serial.printf("%02X ", buf[i]);
+#else	
+		Serial.print(' ');
+		if ((buf[i] & 0xF0) == 0) Serial.print('0');
+		Serial.print(buf[i], HEX);
+#endif
+	}
+}
+
+//int main(void) { setup();
+__ATTR_NORETURN__ void loop() {
+	for (int ret;;) {
 		if (BtnErase.longPress()) {
 			//OLED.print(F("The keys have been deleted"), 0, 0);
 			DEBUGLN(F("The keys have been deleted"));
@@ -74,11 +160,11 @@ int main(void) {
 		//stTimer = millis();
 		switch (Mode) {
 		case md_empty: case md_read:
-			if (!readEM_Marine(buffer)) {
+			if (!readEM_Marine(Buffer)) {
 				keyType = keyEM_Marine;
-			} else if (keyType = KeyDetection(buffer)) {
+			} else if (keyType = obj.KeyDetection(Buffer)) {
 				//keyType = keyCyfral;
-			} else if (read_dallas(buffer)) {
+			} else if (read_dallas(Buffer)) {
 				keyType = keyDallas;
 			} else continue;
 
@@ -88,26 +174,28 @@ int main(void) {
 			OLED_printKey(keyID, true);
 			break;
 		case md_write:
-			if (keyType == keyEM_Marine) { error = write_rfid(keyID); } 
-			else { error = write_ibutton(keyID); }
-			OLEDprint_error(error);
+			if (keyType == keyEM_Marine) { ret = writeRfid(keyID); }
+			else { ret = write_ibutton(keyID); }
+			OLEDprint_error(ret);
 			delay(1000);
 			break;
 		case md_blueMode:
-			SendEM_Marine(keyID);
+			rfid_pwm_disable();  delay(10);// отключаем шим
+			sendEM_Marine(keyID);
 			break;
 		}  //end switch
 
 	}
-	return 0;
+	//return 0;
 }
 
 void setup() {
-	cli();
+	rfid_test();
+/*	cli();
 #ifndef _GYVERCORE_NOMILLIS
 	TIMSK0 |= _BV(TOIE0);
 #endif 
-	sei();
+	sei();*/
 	pinMode(Luse_Led, OUTPUT);
 	digitalWrite(Luse_Led, HIGH);   //поменять на LOW
 	pinMode(BtnOKPin, INPUT_PULLUP);                            // включаем чтение и подягиваем пин кнопки режима к +5В
@@ -117,7 +205,7 @@ void setup() {
 	pinMode(ACpin, INPUT);  // Вход аналогового компаратора 3В для Cyfral
 	//pinMode(R_Led, OUTPUT);pinMode(G_Led, OUTPUT);pinMode(B_Led, OUTPUT);  //RGB-led
 	//clearLed();
-	pinMode(FreqGen, OUTPUT);
+	rfid_init(); //rfid_disable();
 #ifdef DEBUG_ENABLE
 	Serial.begin(115200);
 #endif
@@ -143,7 +231,8 @@ void setup() {
 		OLED_printKey(keyID);
 		Mode = md_read;
 		digitalWrite(G_Led, HIGH);
-	} else {
+	}
+	else {
 		//OLED.print(F("ROM has no keys yet."), 0, 12); OLED.update();
 	}
 	//enc1.setTickMode(AUTO);
@@ -154,3 +243,15 @@ void setup() {
 	//digitalWrite(Luse_Led, !digitalRead(Luse_Led));
 }
 
+void dac_init() {
+#if defined __LGT8F__
+	pinMode(5, OUTPUT); //pulldown for pin 6
+	DACON = _BV(DACEN) | _BV(DAVS1) | _BV(DAOE); //DAC enable; internal reference voltage; output enable pin 4
+	//VCAL = VCAL2; bitSet(ADCSRD, REFS1);//2.048v //ADC REF_V 
+	//VCAL = VCAL3; bitSet(ADCSRD, REFS2); //4.096v
+	//Serial.print("VCAL1 "); Serial.println(VCAL1);Serial.print("VCAL2 "); Serial.println(VCAL2);Serial.print("VCAL3 "); Serial.println(VCAL3);
+	VCAL = VCAL1;
+	DALR = 0xFF;
+	C0SR = _BV(C0BG); //DAC output on positive input //comparator enable
+#endif
+}
